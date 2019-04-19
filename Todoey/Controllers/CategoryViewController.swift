313beccbe055ce51f561f9290c73db7fda45407c
Shaun B <shaungboncker@gmail.com>
2019-04-19 19:44:47 +1000

    add source control

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,123 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by SHAUN BONCKER on 5/4/19.
+//  Copyright © 2019 ShaunB. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class CategoryViewController: UITableViewController {
+    
+    let realm = try! Realm()
+    
+    var categories : Results<Category>?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadCategories()
+        
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return categories?.count ?? 1
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories Added Yet"
+        
+        return cell
+        
+    }
+    
+    //MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+        
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! ToDoListViewController
+        
+        if let indexpath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories?[indexpath.row]
+            
+        }
+    }
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+    func save(category: Category) {
+        do {
+            try realm.write {
+                realm.add(category)
+                }
+            }catch{
+            print("Error saving category \(error)")
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+    func loadCategories() {
+        
+        categories = realm.objects(Category.self)
+        
+        tableView.reloadData()
+        
+        // core data code
+//        let request : NSFetchRequest<Category> = Category.fetchRequest()
+//        do {
+//        categories = try context.fetch(request)
+//        } catch {
+//            print("Error loading categories \(error)")
+//        }
+    }
+ 
+    //MARK: - Add New Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textfield = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            //what will happen once the user clicks the Add item button on our UIAlert
+            
+            let newCategory = Category()
+            newCategory.name = textfield.text!
+            
+            self.save(category: newCategory)
+            
+            
+            
+        }
+        alert.addAction(action)
+        
+        alert.addTextField { (field) in
+            textfield = field
+            textfield.placeholder = "Add new category"
+        }
+        
+         present(alert, animated: true, completion: nil)
+    }
+    
+}
+
+
+
+
+
+
